24c30d6486f9a39ad1e0c60d9669400a5e5806f3
Amber Chunn <a.77@me.com>
2020-05-15 19:00:43 -0700

    Set Up Auth Token Module

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+	return (
+		<>
+			<h1>Welcome to the Bubble App!</h1>
+			<form>
+				<input />
+				<input />
+				<button onClick={}>Login</button>
+			</form>
+		</input>
+	);
 };
 
 export default Login;
